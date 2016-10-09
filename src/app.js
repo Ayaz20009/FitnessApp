7b195fac521672c6427032c2a2ee20c395aa9880
Ayaz Shah <ayazshah_20009@yahoo.com>
2016-10-09 19:38:10 -0400

    Views

--- src/app.js
+++ src/app.js
@@ -1,9 +1,21 @@
 'use strict';
 
-var express = require('express')
+var express = require('express');
+
 var app = express();
 
-app.get('/',function(request,response){
-  response.send("Hello World")
+app.set('view engine','jade');
+app.set('views', __dirname + '/templates');
+
+app.get('/',function(req,res){
+  res.render('index')
 });
-app.listen(3000);
+
+app.get('/blog' ,function(req,res){
+  res.send("<h2>Blog Page </h2>");
+});
+
+
+app.listen(3000,function(){
+  console.log("The frontend server is running on port 3000")
+});
